ce593082eb8d377b61b8c71f8c03d5d35c586a73
Mohammad Khodabandeh <Khodabandeh.Mohammad@gmail.com>
2018-10-10 08:43:15 +0330

    [EDIT] move build scripts to deplyment.sh

---
+++ Avt.Web.Backend/deployment.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+echo "*** adding zip ... ***"  
+apk add zip unzip
+
+echo "*** changing directoriy for command ... ***"  
+cd Avt.Web.Backend
+echo "*** restoring packages ... ***"  
+dotnet restore
+echo "*** building ... ***"  
+dotnet build -c Release
+echo "*** publishing ... ***"  
+dotnet publish --no-restore -c Release
+
+echo "*** Creating suffix for file name ... ***"  
+
+tail=< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${5:-16};
+
+echo "*** Zipping the artifact ... ***"  
+
+zip -r "$backend_$tail.zip" bin/Release/netcoreapp2.1/publish/
+
+echo "*** All Finished :) ... ***"  
+
